0a36f064db34ead0887656bcc001d61ac85494a9
cgcladera <cgcladera@gmail.com>
2013-10-25 14:13:13 +0200

    Imports template before execute composer

--- wsapp/recipes/appsetup.rb
+++ wsapp/recipes/appsetup.rb
@@ -1,4 +1,30 @@
 node[:deploy].each do |app_name, deploy|
+  template "#{deploy[:deploy_to]}/current/app/config/parameters.yml" do
+      source "parameters.yml.erb"
+      mode 0660
+      group deploy[:group]
+
+      if platform?("ubuntu")
+        owner "www-data"
+      elsif platform?("amazon")
+        owner "apache"
+      end
+
+      variables(
+        :host =>     (deploy[:database][:host] rescue nil),
+        :user =>     (deploy[:database][:username] rescue nil),
+        :password => (deploy[:database][:password] rescue nil),
+        :db =>       (deploy[:database][:database] rescue nil),
+        :domain =>    (node[:wsapp][:domain] rescue nil),
+        :media_bucket => (node[:wsmediabucket] rescue nil),
+        :aws_access_key => (node[:awsaccess] rescue nil),
+        :aws_secret_key => (node[:awssecret] rescue nil)
+      )
+
+     only_if do
+       File.directory?("#{deploy[:deploy_to]}/current")
+     end
+  end
   script "install_composer" do
     interpreter "bash"
     user "root"
@@ -7,32 +33,5 @@ node[:deploy].each do |app_name, deploy|
     curl -s https://getcomposer.org/installer | php
     php composer.phar install
     EOH
-  end
-
-  template "#{deploy[:deploy_to]}/current/app/config/parameters.yml" do
-    source "parameters.yml.erb"
-    mode 0660
-    group deploy[:group]
-
-    if platform?("ubuntu")
-      owner "www-data"
-    elsif platform?("amazon")   
-      owner "apache"
-    end
-
-    variables(
-      :host =>     (deploy[:database][:host] rescue nil),
-      :user =>     (deploy[:database][:username] rescue nil),
-      :password => (deploy[:database][:password] rescue nil),
-      :db =>       (deploy[:database][:database] rescue nil),
-      :domain =>    (node[:wsapp][:domain] rescue nil),
-      :media_bucket => (node[:wsmediabucket] rescue nil),
-      :aws_access_key => (node[:awsaccess] rescue nil),
-      :aws_secret_key => (node[:awssecret] rescue nil)
-    )
-
-   only_if do
-     File.directory?("#{deploy[:deploy_to]}/current")
-   end
   end
 end
